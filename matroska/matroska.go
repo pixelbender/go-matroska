1194444fe603123b77f4fa93cbf30eba09eeee4b
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-04-18 22:56:23 +0200

    Remove useless logs

--- matroska/matroska.go
+++ matroska/matroska.go
@@ -1,10 +1,10 @@
 package matroska
 
 import (
-	"github.com/pixelbender/go-matroska/ebml"
-	"log"
 	"os"
 	"time"
+
+	"github.com/pixelbender/go-matroska/ebml"
 )
 
 func Decode(file string) (*File, error) {
@@ -145,31 +145,29 @@ const (
 )
 
 func (b *Block) UnmarshalEBML(r *ebml.Reader) error {
-	//log.Printf("\t Block %#v", r.Len())
 	v, err := r.ReadVInt()
 	if err != nil {
-		log.Println(err)
 		return err
 	}
+
 	b.TrackNumber = TrackNumber(v)
 	p, err := r.Next(3)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
+
 	b.Timecode = int16(p[0])<<8 | int16(p[1])
 	b.Flags = p[2]
 	if (b.Flags>>1)&3 == 0 {
 		return nil
 	}
-	log.Printf("\t Block %#v", b)
+
 	p, err = r.Next(1)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
 	b.Frames = int(p[0])
-	log.Printf("\t Block %#v", b)
+
 	return nil
 }
 
